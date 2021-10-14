bec62a848f0ac94f7d9f23fb339ff67d49aedb0b
unknown <math3226@gmail.com>
2021-10-14 20:59:14 -0300

    Completed dropdown menu task

--- javascript-DOM/dropdown-menu/js/modules/dropdown-menu.js
+++ javascript-DOM/dropdown-menu/js/modules/dropdown-menu.js
@@ -1,10 +1,16 @@
+import outsideClick from "./outsideclick.js";
+
 export default function initDropDownMenu() {
   const dropdownMenus = document.querySelectorAll('[data-dropdown]');
 
   function handleClick(e) {
     e.preventDefault();
 
-    this.classList.toggle('active');
+    this.classList.add('active');
+
+    outsideClick(this, ['click', 'touchstart'],() => {
+      this.classList.remove('active')
+    });
   }
 
   dropdownMenus.forEach((menu) => {
